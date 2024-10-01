195e87708ac7381f1e0f11617b423f39d1d1163c
Shay <shaybush93@gmail.com>
2024-10-01 14:55:58 +0300

    gcd

---
+++ gcd.py
@@ -0,0 +1,10 @@
+def gcd(a, b):
+  max_gcd = 0
+  if b > a:
+    [a,b] = [b,a]
+  for current_num in range(1,b + 1):
+    if b % current_num == 0 and a % current_num == 0:
+      max_gcd = current_num
+  return max_gcd
+
+print(f"gcd - {gcd(6,12)}")
